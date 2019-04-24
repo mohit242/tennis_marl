fix: gradient clip not was not used, layer_init for actor critic

tennis_maddpg/maddpg_agent.py
@@ -1,17 +1,18 @@
+from comet_ml import Experiment
 import torch
+import torch.nn.functional as F
 import numpy as np
 from .network import *
 from .utils import *
 import sys
 import logging
-import wandb
-
-wandb.init(project="tennis_marl")
-
+# import wandb
+experiment = Experiment(api_key="FhiGGed6g73CWKq7YS2AEDSaL",
+                        project_name="tennis_marl", workspace="drl")
 class MADDPGAgent:
 
-    def __init__(self, env, start_steps=1000, train_after_every=20, steps_per_epoch=10, gradient_clip=1, gamma=0.95, device='cpu',
-                 minibatch_size=256, buffer_size=10e5, polyak=0.01):
+    def __init__(self, env, start_steps=1000, train_after_every=20, steps_per_epoch=10, gradient_clip=2, gamma=0.95,
+                 device='cpu',minibatch_size=256, buffer_size=10e5, polyak=0.01):
         super().__init__()
         self.device = device
         self.minibatch_size = minibatch_size
@@ -31,34 +32,38 @@ class MADDPGAgent:
         # For this particular scenario we are using the same actor for both agents
         self.actor = DDPGActor(self.state_dim, self.action_dim)
         self.critic = DDPGCritic(self.state_dim, self.action_dim)
-        self.critic_opt = torch.optim.Adam(self.critic.parameters(), lr=0.01)
-        self.actor_opt = torch.optim.Adam(self.actor.parameters(), lr=0.01)
+        self.critic_opt = torch.optim.Adam(self.critic.parameters(), lr=3e-4, weight_decay=0.0001)
+        self.actor_opt = torch.optim.Adam(self.actor.parameters(), lr=1e-4)
         self.replay_buffer = ReplayBuffer(self.action_dim, int(buffer_size), minibatch_size)
 
-        wandb.watch((self.critic, self.actor))
-
+        #wandb.watch((self.critic, self.actor))
+        hyperparameters = {"start_steps": start_steps, "train_after_every": train_after_every,
+                           "steps_per_epoch": steps_per_epoch, "gradient_clip": gradient_clip, "gamma": gamma,
+                           "minibatch_size": minibatch_size, "polyak": polyak}
+        experiment.log_multiple_params(hyperparameters)
         self.actor_target = DDPGActor(self.state_dim, self.action_dim)
         self.critic_target = DDPGCritic(self.state_dim, self.action_dim)
         self.step_counter = 0
-        for target_param, local_param in zip(self.actor_target.parameters(), self.actor.parameters()):
-            target_param.data.copy_(local_param.data)
-
-        for target_param, local_param in zip(self.critic_target.parameters(), self.critic.parameters()):
-            target_param.data.copy_(local_param.data)
+        # for target_param, local_param in zip(self.actor_target.parameters(), self.actor.parameters()):
+        #     target_param.data.copy_(local_param.data)
+        #
+        # for target_param, local_param in zip(self.critic_target.parameters(), self.critic.parameters()):
+        #     target_param.data.copy_(local_param.data)
 
     def learn_step(self):
         if len(self.replay_buffer) < self.start_steps:
             env_info = self.env.reset(train_mode=True)[self.brain_name]
             state = env_info.vector_observations
             for _ in range(self.start_steps):
-                action = np.random.randn(self.num_agents, self.action_dim)
-                action = np.clip(action, -1, 1)
+                action = np.random.uniform(-1, 1, (self.num_agents, self.action_dim))
+                # action = np.clip(action, -1, 1)
                 env_info = self.env.step(action)[self.brain_name]
                 next_state = env_info.vector_observations
-                reward = env_info.rewards
-                done = env_info.local_done
-                for s, a, r, ns, d in zip(state, action, reward, next_state, done):
-                    self.replay_buffer.add(s, a, r, ns, d)
+                reward = np.array(env_info.rewards)
+                done = np.array(env_info.local_done)
+                # for s, a, r, ns, d in zip(state, action, reward, next_state, done):
+                #     self.replay_buffer.add(s, a, r, ns, d)
+                self.replay_buffer.add(state, action, reward, next_state, done)
                 state = next_state
                 if np.any(done):
                     env_info = self.env.reset(train_mode=True)[self.brain_name]
@@ -75,28 +80,32 @@ class MADDPGAgent:
             reward = env_info.rewards
             score += reward
             done = env_info.local_done
-            for s, a, r, ns, d in zip(state, action.detach().numpy(), reward, next_state, done):
-                self.replay_buffer.add(s, a, r, ns, d)
+            # for s, a, r, ns, d in zip(state, action.detach().numpy(), reward, next_state, done):
+            #     self.replay_buffer.add(s, a, r, ns, d)
+            self.replay_buffer.add(state, action.detach().numpy(), reward, next_state, done)
             state = next_state
             self.step_counter += 1
             if self.step_counter % self.train_after_every == 0:
                 for epc in range(self.steps_per_epoch):
                     states, actions, rewards, next_states, dones = self.replay_buffer.sample()
-                    targets = rewards + self.gamma * (1 - dones) * self.critic_target(next_states,
-                                                                                      self.actor_target(next_states))
-                    critic_loss = torch.mean(torch.pow(self.critic(states.float(), actions.float()) - targets, 2))
+                    q_fut = self.critic_target(next_states, self.actor_target(next_states)).squeeze(-1)
+                    targets = rewards + self.gamma * (1 - dones) * q_fut
+                    critic_loss = F.mse_loss(self.critic(states.float(), actions.float()).squeeze(-1), targets)
 
                     self.critic_opt.zero_grad()
                     critic_loss.backward()
+                    torch.nn.utils.clip_grad_norm_(self.critic.parameters(), self.gradient_clip)
                     self.critic_opt.step()
                     # if epc % 2 == 0:
                     if True:
-                        actor_loss = -torch.mean(self.critic(states, self.actor(states)))
+                        actor_loss = -torch.mean(self.critic(states.detach(), self.actor(states)))
 
                         self.actor_opt.zero_grad()
                         actor_loss.backward()
                         self.actor_opt.step()
-                        wandb.log({"critic_loss": critic_loss, "actor_loss": actor_loss})
+                        experiment.log_metric("critic_loss", critic_loss)
+                        experiment.log_metric("actor_loss", actor_loss)
+                        #wandb.log({"critic_loss": critic_loss, "actor_loss": actor_loss})
                     self.soft_update()
 
             if np.any(done):

tennis_maddpg/utils.py
@@ -50,11 +50,17 @@ class ReplayBuffer:
         experiences = random.sample(self.memory, k=self.batch_size)
 
 
-        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float()
-        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long()
-        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float()
-        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float()
-        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float()
+        # states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float()
+        # actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long()
+        # rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float()
+        # next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float()
+        # dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float()
+
+        states = torch.from_numpy(np.array([e.state for e in experiences if e is not None])).float()
+        actions = torch.from_numpy(np.array([e.action for e in experiences if e is not None])).long()
+        rewards = torch.from_numpy(np.array([e.reward for e in experiences if e is not None])).float()
+        next_states = torch.from_numpy(np.array([e.next_state for e in experiences if e is not None])).float()
+        dones = torch.from_numpy(np.array([e.done for e in experiences if e is not None]).astype(np.uint8)).float()
 
         return states, actions, rewards, next_states, dones
 

train.py
@@ -7,9 +7,9 @@ import torch as torch
 
 
 if __name__=="__main__":
-    env = UnityEnvironment(file_name="Tennis_Linux/Tennis.x86")
+    env = UnityEnvironment(file_name="/home/mohit/continuous_control/Reacher_Linux/Reacher.x86")
     agent = MADDPGAgent(env, train_after_every=100, gamma=0.95, minibatch_size=128, steps_per_epoch=20,
-                        start_steps=10000)
+                        start_steps=10000, polyak=0.005, gradient_clip=0.1)
     torch.manual_seed(0)
     np.random.seed(0)
     scores = []
@@ -22,8 +22,6 @@ if __name__=="__main__":
         print("\rEpisode- {:8d} \t Score- {:+8f} \t Mean Score- {:+8f}".format(i, score, np.mean(scores_window)), end="")
         if i%100 == 0:
             print("\rEpisode- {:8d} \t Score- {:+8f} \t Mean Score- {:+8f}".format(i, score, np.mean(scores_window)))
-        if np.mean(scores_window) >= 0.5:
-            break
-        if i == 2000:
+        if np.mean(scores_window) >= 30:
             break
         i += 1